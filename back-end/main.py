19017c1d657059c6f6c112bddb04b9198405e4bf
wearecomming <96563631+wearecomming@users.noreply.github.com>
2023-11-24 21:58:10 +0800

    add first spring

---
+++ back-end/main.py
@@ -0,0 +1,6 @@
+from model import app,db
+from user import user_api
+app.register_blueprint(user_api)
+if __name__ == '__main__':
+    db.create_all()
+    app.run(host="0.0.0.0", port=443, debug=True)
